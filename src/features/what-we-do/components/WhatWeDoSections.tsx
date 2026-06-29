83221c6626abd80f869ab90e1be13fbacdc23cf8
Aqib Bashir <itzaqibbashir@gmail.com>
2026-06-29 18:51:14 +0530

    fixed border for what we do

--- src/features/what-we-do/components/WhatWeDoSections.tsx
+++ src/features/what-we-do/components/WhatWeDoSections.tsx
@@ -2,25 +2,30 @@ import HeadingPill from "@/components/typography/headingPill";
 import { WHAT_WE_DO_DATA } from "@/constants/what-we-do";
 import WhatWeDoSection from "./WhatWeDoSection";
 
-
 function WhatWeDoSections() {
   return (
     <>
-      {WHAT_WE_DO_DATA.map((WHAT_WE_DO) => {
+      {WHAT_WE_DO_DATA.map((WHAT_WE_DO, index) => {
+        // Check if this is the last item in the array
+        const isLast = index === WHAT_WE_DO_DATA.length - 1;
+
         return (
           <section
             id={WHAT_WE_DO.title.toLowerCase()}
             key={WHAT_WE_DO.title}
-            className="max-w-max mx-auto px-x ]"
+            className="max-w-max mx-auto px-x "
           >
-            <div className="py-y border-b last-of-type:border-b-0 border-[#BDBDBD] ">
+            {/* Conditionally apply the border if it's NOT the last item */}
+            <div
+              className={`py-y ${!isLast ? "border-b border-[#BDBDBD]" : ""}`}
+            >
               <HeadingPill>{WHAT_WE_DO.title}</HeadingPill>
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-15 lg:gap-25 mt-between-content">
-                {WHAT_WE_DO.childrens.map((item, index) => {
+                {WHAT_WE_DO.childrens.map((item, childIndex) => {
                   return (
                     <WhatWeDoSection
-                      key={`${item.heading.text}-${index}`}
-                      index={index}
+                      key={`${item.heading.text}-${childIndex}`}
+                      index={childIndex}
                       data={item}
                     />
                   );
@@ -34,4 +39,4 @@ function WhatWeDoSections() {
   );
 }
 
-export default WhatWeDoSections
+export default WhatWeDoSections;
