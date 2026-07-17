0db657f461a78b4eb38daa28a59723d6a4e201e4
Aqib Bashir <itzaqibbashir@gmail.com>
2026-07-17 21:45:53 +0530

    added site map and privacy policy in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -84,19 +84,19 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     //   priority: 0.6,
     // },
     //
-    // {
-    //   url: `${SITE_URL}/privacy-policy`,
-    //   lastModified: now,
-    //   changeFrequency: "yearly",
-    //   priority: 0.3,
-    // },
-    //
-    // {
-    //   url: `${SITE_URL}/terms-and-conditions`,
-    //   lastModified: now,
-    //   changeFrequency: "yearly",
-    //   priority: 0.3,
-    // },
+    {
+      url: `${SITE_URL}/privacy-policy`,
+      lastModified: now,
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    
+    {
+      url: `${SITE_URL}/terms-and-conditions`,
+      lastModified: now,
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
   ];
 
   routes.push(
