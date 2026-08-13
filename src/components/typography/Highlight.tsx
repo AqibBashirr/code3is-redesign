64922931a0360935f171604291a16ab612586e98
Aqib Bashir <itzaqibbashir@gmail.com>
2026-08-13 11:36:54 +0530

    fixed the accessiblity issue for highlight underline

--- src/components/typography/Highlight.tsx
+++ src/components/typography/Highlight.tsx
@@ -1,16 +1,22 @@
 import { cn } from "@/constants/utils";
 import React from "react";
 
-interface HighlightTextProps{
-  children:React.ReactNode;
-  className?:string;
+interface HighlightTextProps {
+  children: React.ReactNode;
+  className?: string;
 }
-function HighlightText({ children,className }: HighlightTextProps) {
+function HighlightText({ children, className }: HighlightTextProps) {
   return (
-    <span className={cn("underline decoration-[#a3e635] decoration-4 underline-offset-8 font-semibold",className)}>
+    <strong
+      className={cn(
+        "relative font-semibold italic",
+        "before:absolute before:inset-x-0 before:-bottom-1 before:-z-10 before:h-1 not-italic before:bg-[#a3e635] before:content-['']",
+        className,
+      )}
+    >
       {children}
-    </span>
+    </strong>
   );
 }
 
-export default HighlightText
+export default HighlightText;
